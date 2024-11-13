fc813a01c9a5ed912f398184b4ab239b10ebfc24
matthew <mdenslinger@ufl.edu>
2024-11-12 21:44:21 -0500

    Add help icon to outputconfig settings

--- src/Components/OutputConfigs/OutputFileConfig.jsx
+++ src/Components/OutputConfigs/OutputFileConfig.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import FlextextConfig from "./FlextextConfig.jsx";
 import ElanConfig from "./ElanConfig.jsx";
+import styles from "./OutputFileConfig.module.css";
+import { MdOutlineHelp } from "react-icons/md";
 
 function OutputFileConfig({
   isOpen,
@@ -12,25 +14,35 @@ function OutputFileConfig({
   includedLayerValues,
   setIncludedLayerValues,
   elanTemplateFileName,
-  setElanTemplateFileName
+  setElanTemplateFileName,
 }) {
+  //help info tooltip display text
+  let helpTextInfo = "";
 
   function getDialogConfigTitle() {
     let dialogConfigTitle = "";
 
     if (data.outFileType === "flextext") {
       dialogConfigTitle = "Flextext";
+      helpTextInfo = "Open Flextext Settings Help";
     } else if (data.outFileType === "elan") {
       dialogConfigTitle = "ELAN";
+      helpTextInfo = "Open Elan Settings Help";
     }
 
     return dialogConfigTitle;
   }
-  
+
   return (
     <dialog open={isOpen} className="modal-overlay">
       <article>
-        <h2>{getDialogConfigTitle()} Configuration Settings</h2>
+        <h2>{getDialogConfigTitle()} Configuration Settings</h2>{" "}
+        <a data-tooltip={helpTextInfo} data-placement="bottom">
+          <MdOutlineHelp
+            className={styles.helpIcon}
+            onClick={() => window.pythonApi.openHelpWindow()}
+          />
+        </a>
         <hr></hr>
         <section>
           {data.outFileType === "flextext" && (
